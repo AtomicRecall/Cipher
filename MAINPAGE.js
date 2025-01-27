4b208cce418bedba728398ff104ae1d2e08b8860
atomic <danielyurskiyimportant@gmail.com>
2025-01-27 11:36:59 -0500

    FUCK

--- MAINPAGE.js
+++ MAINPAGE.js
@@ -993,7 +993,7 @@ function getUpcomingMatches(team,season,upcomingdivider){
     switch (season){
         case 52:
                 
-    return fetch(`https://cipher-virid.vercel.app/api/proxy?endpoint=/championships/v1/matches?participantId=`+team+`&participantType=TEAM&championshipId=c9f295b8-f68d-492b-bc38-75628dd91103&limit=20&offset=0`,{
+    return fetch(`https://cipher-virid.vercel.app/api/proxy?endpoint=championships/v1/matches?participantId=`+team+`&participantType=TEAM&championshipId=c9f295b8-f68d-492b-bc38-75628dd91103&limit=20&offset=0`,{
         headers:{
             'Access-Control-Allow-Origin' : '*'
         }
