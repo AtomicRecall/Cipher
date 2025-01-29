2939ed9cc2811a6efb02d4346dedb21a8f8c2f18
atomic <danielyurskiyimportant@gmail.com>
2025-01-29 16:23:36 -0500

    Alpha v1.3.3 - Upcoming Matches + Full website fix

--- api/proxy2.js
+++ api/proxy2.js
@@ -8,11 +8,12 @@ export default async function handler(req, res) {
   const apiUrl = `https://api.faceit.com/democracy/v1/match/${url}`;
 
   console.log("Requesting data from:", apiUrl); // Debug log to verify the URL
-  res.setHeader("Access-Control-Allow-Origin", "*"); // Enable CORS
+
   try {
     const response = await axios.get(apiUrl);
     console.log("Data received:", response.data); // Log the received data
-    
+    req.setHeader("Access-Control-Allow-Headers", "*");
+    res.setHeader("Access-Control-Allow-Origin", "*"); // Enable CORS
 
     res.status(200).json(response.data); // Send back the data
     return response.data;
