69c83d55da234c6f655676e01367b1e91bbda0e0
atomic <danielyurskiyimportant@gmail.com>
2025-01-29 13:11:35 -0500

    IM SO CLOSE PLEASE WORK

--- api/proxy2.js
+++ api/proxy2.js
@@ -14,8 +14,11 @@ app.use(
       const fullUrl = new URL(req.originalUrl, `http://${req.headers.host}`);
       const queryString = fullUrl.searchParams.toString(); // Preserve full query string
 
-      console.log(`Proxying request to: https://api.faceit.com/${originalPath}?${queryString}`);
-      return `/${originalPath}?${queryString}`;
+      const endpoint = req.query.endpoint || '';
+      const apiUrl = `https://api.faceit.com/${endpoint}`;
+      console.log(`Proxying request to: ${apiUrl}`);
+
+      return `${endpoint}`;
     },
     proxyReqOptDecorator: (proxyReqOpts) => {
       proxyReqOpts.headers["Authorization"] = `Bearer 1df284f3-de17-4d2e-b8c7-5a460265e05a`;
