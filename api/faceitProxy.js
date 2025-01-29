20e9f53954441252b5aeb50e62da770ba7cb3a34
atomic <danielyurskiyimportant@gmail.com>
2025-01-29 14:43:52 -0500

    24342

--- api/faceitProxy.js
+++ api/faceitProxy.js
@@ -20,6 +20,7 @@ export default async function handler(req, res) {
     res.setHeader("Access-Control-Allow-Origin", "*"); 
     res.status(200).json(response.data); 
     */
+   return response.data;
   } catch (error) {
     console.error("Error fetching data:", error.message);
     res.status(error.response?.status || 500).json({ error: "Error fetching data from Faceit API" });
