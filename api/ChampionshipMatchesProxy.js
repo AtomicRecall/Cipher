d97cca01bf03dd95db73a156de1a537be2fb9346
atomic <66749320+AtomicRecall@users.noreply.github.com>
2025-09-23 17:18:51 -0400

    Add files via upload

---
+++ api/ChampionshipMatchesProxy.js
@@ -0,0 +1,23 @@
+// api/faceitProxy.js
+import axios from 'axios';
+
+export default async function handler(req, res) {
+  const { teamId, champID } = req.query; 
+  const apiUrl = `https://api.faceit.com/championships/v1/matches?participantId=${teamId}&participantType=TEAM&championshipId=${champID}&limit=20`
+
+  console.log("Requesting data from:", apiUrl); // Debug log to verify the URL
+
+  try {
+    const response = await axios.get(apiUrl);
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    res.setHeader("Access-Control-Allow-Origin", "*"); 
+    console.log("Data received:", response.data); // Log the received data
+
+    res.status(200).json(response.data); // Send back the data
+    return response.data;
+
+  } catch (error) {
+    console.error("Error fetching data:", error.message);
+    res.status(error.response?.status || 500).json({ error: "Error fetching data from Faceit API" });
+  }
+}
---
+++ api/TeamLeagueProxy.js
@@ -0,0 +1,22 @@
+// api/faceitProxy.js
+import axios from 'axios';
+
+export default async function handler(req, res) {
+  const apiUrl = `https://www.faceit.com/api/team-leagues/v1/teams/${url}/profile/leagues/summary`;
+
+  console.log("Requesting data from:", apiUrl); // Debug log to verify the URL
+
+  try {
+    const response = await axios.get(apiUrl);
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    res.setHeader("Access-Control-Allow-Origin", "*"); 
+    console.log("Data received:", response.data); // Log the received data
+
+    res.status(200).json(response.data); // Send back the data
+    return response.data;
+
+  } catch (error) {
+    console.error("Error fetching data:", error.message);
+    res.status(error.response?.status || 500).json({ error: "Error fetching data from Faceit API" });
+  }
+}
